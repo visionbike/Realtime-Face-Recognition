d6b88a1370464adb082926919c209eb6d7b3e534
visionbike <phuc.visionbike@gmail.com>
2026-06-24 14:31:51 +0800

    fix the conflit

--- app/export_arcface_onnx.py
+++ app/export_arcface_onnx.py
@@ -8,13 +8,13 @@ sys.path.insert(0, str(ROOT_DIR))
 
 from core.recognizer.arcface import iresnet_inference
 
-model = iresnet_inference("r50", "../weights/recognition/arcface_r100.pth", "cpu")
+model = iresnet_inference("r50", "weights/recognition/arcface_r100.pth", "cpu")
 dummy = torch.randn(1, 3, 112, 112)
 
 torch.onnx.export(
     model,
     (dummy,),
-    "../weights/recognition/arcface_r100_int8.onnx",
+    "weights/recognition/arcface_r100_int8.onnx",
     dynamo=True,
     external_data=False,        # keep a single inline .onnx file
     input_names=["x"], output_names=["embedding"],
